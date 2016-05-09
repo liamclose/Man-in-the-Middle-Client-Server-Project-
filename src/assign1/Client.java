dac4db72339d4c2c2fb77ca1424d0aa1fcc084d6
megardron <meganardron@cmail.carleton.ca>
2016-05-09 14:34:39 -0400

    client has the better exit

--- src/assign1/Client.java
+++ src/assign1/Client.java
@@ -7,7 +7,9 @@ public class Client {
 
 	DatagramPacket sendPacket, receivePacket;
 	DatagramSocket sendReceiveSocket;
-	
+	boolean shutdown = false;
+	boolean timeout = true;
+
 	public static final int read = 1;
 	public static final int write = 2;
 
@@ -21,9 +23,9 @@ public class Client {
 			System.exit(1);
 		}
 	}
-	
+
 	public void setShutdown() {
-		sendReceiveSocket.close();
+		shutdown = true;
 	}
 
 	/*
@@ -32,53 +34,59 @@ public class Client {
 	 * Once it has sent the request it waits for the server to respond.
 	 */
 	public void sendAndReceive(int opcode) {
-		String s = "test.txt";
-		String format = "ocTeT";
-		byte msg[] = Message.formatRequest(s, format, opcode);
+		while (timeout&&!shutdown) {
+			timeout = false;
+			String s = "test.txt";
+			String format = "ocTeT";
+			byte msg[] = Message.formatRequest(s, format, opcode);
 
-		try {
-			sendPacket = new DatagramPacket(msg, msg.length,
-					InetAddress.getLocalHost(), 6001);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		Message.printOutgoing(sendPacket, "Client");
+			try {
+				sendPacket = new DatagramPacket(msg, msg.length,
+						InetAddress.getLocalHost(), 6001);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			Message.printOutgoing(sendPacket, "Client");
 
-		// Send the datagram packet to the server via the send/receive socket. 
-		try {
-			sendReceiveSocket.send(sendPacket);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
+			// Send the datagram packet to the server via the send/receive socket. 
+			try {
+				sendReceiveSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			// Construct a DatagramPacket for receiving packets up 
+			// to 100 bytes long (the length of the byte array).
+			byte data[] = new byte[100];
+			receivePacket = new DatagramPacket(data, data.length);
+			while (!shutdown) {
+				try {
+					// Block until a datagram is received via sendReceiveSocket. 
+					sendReceiveSocket.setSoTimeout(3000);
+					sendReceiveSocket.receive(receivePacket);
+				} 
+				catch (SocketTimeoutException e) {
+					timeout = true;
+				}
+				catch(IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
 		}
-		// Construct a DatagramPacket for receiving packets up 
-		// to 100 bytes long (the length of the byte array).
-		byte data[] = new byte[100];
-		receivePacket = new DatagramPacket(data, data.length);
+		if (!shutdown) {
+			// Process the received datagram.
+			Message.printIncoming(receivePacket, "Client");
+		}
 
-		try {
-			// Block until a datagram is received via sendReceiveSocket.  
-			sendReceiveSocket.receive(receivePacket);
-		} 
-		catch (SocketException e) {
-			
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-
-		// Process the received datagram.
-		Message.printIncoming(receivePacket, "Client");
-		
 	}
 
 	public static void main(String args[])
 	{
 		Client c = new Client();
 		new Message(c).start();
-		c.sendAndReceive(read); //for the first 10 requests, send an opcode of either 0 or 1
+		c.sendAndReceive(read); 
 		c.sendReceiveSocket.close();
 	}
 }
